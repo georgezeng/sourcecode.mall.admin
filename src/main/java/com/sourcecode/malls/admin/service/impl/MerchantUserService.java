4228000ffb8ef50c8964910029261fd14244d2de
georgezeng <zzeng@bglcorp.com.au>
2019-03-29 20:45:05 +0800

    no message

--- src/main/java/com/sourcecode/malls/admin/service/impl/MerchantUserService.java
+++ src/main/java/com/sourcecode/malls/admin/service/impl/MerchantUserService.java
@@ -3,6 +3,7 @@ package com.sourcecode.malls.admin.service.impl;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
@@ -18,20 +19,23 @@ public class MerchantUserService {
 	@Autowired
 	private UserRepository repository;
 
-	public void register(User merchant, String validCode) {
+	@Autowired
+	private PasswordEncoder pwdEncoder;
+
+	public void register(User merchant) {
 		AssertUtil.assertNotNull(merchant, "没有提交用户数据");
 		AssertUtil.assertTrue(!StringUtils.isEmpty(merchant.getUsername()), "账号不能为空");
 		AssertUtil.assertTrue(RegexpUtil.matchMobile(merchant.getUsername()), "账号长度必须是11位且全部是数字");
 		AssertUtil.assertTrue(!StringUtils.isEmpty(merchant.getEmail()), "邮箱不能为空");
 		AssertUtil.assertTrue(RegexpUtil.matchEmail(merchant.getEmail()), "邮箱格式不正确");
-		AssertUtil.assertTrue(!StringUtils.isEmpty(validCode) && validCode.equals(merchant.getVerifyCode()), "验证码无效");
 		AssertUtil.assertTrue(!StringUtils.isEmpty(merchant.getPassword()), "密码不能为空");
 		AssertUtil.assertTrue(RegexpUtil.matchPassword(merchant.getPassword()), "密码必须数字+字母（区分大小写）并且不少于8位");
 		AssertUtil.assertTrue(merchant.getPassword().equals(merchant.getConfirmPassword()), "确认密码与密码不一致");
 		Optional<User> existedUser = repository.findByUsername(merchant.getUsername());
 		AssertUtil.assertTrue(!existedUser.isPresent(), "用户已存在");
 		merchant.setEnabled(true);
+		merchant.setPassword(pwdEncoder.encode(merchant.getPassword()));
 		repository.save(merchant);
 	}
-	
+
 }
--- src/main/java/com/sourcecode/malls/admin/web/controller/MerchantUserController.java
+++ src/main/java/com/sourcecode/malls/admin/web/controller/MerchantUserController.java
@@ -1,5 +1,7 @@
 package com.sourcecode.malls.admin.web.controller;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -8,21 +10,24 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alibaba.druid.util.StringUtils;
+import com.sourcecode.malls.admin.domain.User;
 import com.sourcecode.malls.admin.domain.redis.CodeStore;
 import com.sourcecode.malls.admin.dto.base.ResultBean;
 import com.sourcecode.malls.admin.repository.redis.impl.CodeStoreRepository;
 import com.sourcecode.malls.admin.service.impl.MerchantUserService;
 import com.sourcecode.malls.admin.service.impl.SmsService;
+import com.sourcecode.malls.admin.util.AssertUtil;
 import com.sourcecode.malls.admin.util.CodeUtil;
 
 @RestController
 @RequestMapping(path = "/merchant")
 public class MerchantUserController {
 	private static final String REGISTER_CODE_KEY = "merchant-register-code";
+	private static final String REGISTER_CODE_TIME_ATTR = "merchant-register-code-time";
 
 	@Autowired
 	private MerchantUserService userService;
@@ -35,14 +40,31 @@ public class MerchantUserController {
 
 	@RequestMapping(path = "/register/code/{mobile}")
 	public ResultBean<Void> getRegisterVerifyCode(@PathVariable String mobile, HttpSession session) {
-		Optional<CodeStore> codeStoreOp = codeStoreRepository.findByCategoryAndKey(REGISTER_CODE_KEY, session.getId());
-		CodeStore codeStore = codeStoreOp.orElse(new CodeStore(REGISTER_CODE_KEY, session.getId(), CodeUtil.generateRandomNumbers(6)));
+		Date sendTime = (Date) session.getAttribute(REGISTER_CODE_TIME_ATTR);
+		if (sendTime != null) {
+			Calendar c = Calendar.getInstance();
+			c.setTime(sendTime);
+			c.add(Calendar.SECOND, 30);
+			AssertUtil.assertTrue(new Date().after(c.getTime()), "操作太频繁，请稍后重试");
+		}
+		Optional<CodeStore> codeStoreOp = codeStoreRepository.findByCategoryAndKey(REGISTER_CODE_KEY, mobile);
+		CodeStore codeStore = codeStoreOp.orElse(new CodeStore(REGISTER_CODE_KEY, mobile, CodeUtil.generateRandomNumbers(6)));
 		if (codeStore.getId() == null) {
 			codeStoreRepository.save(codeStore);
 		}
 		Map<String, Object> payload = new HashMap<>();
 		payload.put("code", codeStore.getValue());
-		smsService.send("", mobile, payload);
+		smsService.send("SMS_162450479", mobile, payload);
+		session.setAttribute(REGISTER_CODE_TIME_ATTR, new Date());
+		return new ResultBean<>();
+	}
+
+	@RequestMapping(path = "/register/{code}")
+	public ResultBean<Void> register(@RequestBody User merchant, @PathVariable String code) {
+		Optional<CodeStore> codeStoreOp = codeStoreRepository.findByCategoryAndKey(REGISTER_CODE_KEY, merchant.getMobile());
+		AssertUtil.assertTrue(codeStoreOp.isPresent(), "验证码已过期");
+		AssertUtil.assertTrue(codeStoreOp.get().getValue().equals(code), "验证码无效");
+		userService.register(merchant);
 		return new ResultBean<>();
 	}
 }
