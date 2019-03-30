89fa7235f162f0f9afd8b7756c65eccbc215ef5d
georgezeng <zzeng@bglcorp.com.au>
2019-03-30 15:19:40 +0800

    no message

--- src/main/java/com/sourcecode/malls/admin/service/impl/MerchantUserService.java
+++ src/main/java/com/sourcecode/malls/admin/service/impl/MerchantUserService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.StringUtils;
 
 import com.sourcecode.malls.admin.constants.SystemConstant;
 import com.sourcecode.malls.admin.domain.Role;
@@ -34,11 +33,7 @@ public class MerchantUserService {
 
 	public void register(User merchant) {
 		AssertUtil.assertNotNull(merchant, "没有提交用户数据");
-		AssertUtil.assertTrue(!StringUtils.isEmpty(merchant.getUsername()), "账号不能为空");
 		AssertUtil.assertTrue(RegexpUtil.matchMobile(merchant.getUsername()), "账号长度必须是11位且全部是数字");
-		AssertUtil.assertTrue(!StringUtils.isEmpty(merchant.getEmail()), "邮箱不能为空");
-		AssertUtil.assertTrue(RegexpUtil.matchEmail(merchant.getEmail()), "邮箱格式不正确");
-		AssertUtil.assertTrue(!StringUtils.isEmpty(merchant.getPassword()), "密码不能为空");
 		AssertUtil.assertTrue(RegexpUtil.matchPassword(merchant.getPassword()), "密码必须数字+字母（区分大小写）并且不少于8位");
 		AssertUtil.assertTrue(merchant.getPassword().equals(merchant.getConfirmPassword()), "确认密码与密码不一致");
 		Optional<User> existedUser = repository.findByUsername(merchant.getUsername());
